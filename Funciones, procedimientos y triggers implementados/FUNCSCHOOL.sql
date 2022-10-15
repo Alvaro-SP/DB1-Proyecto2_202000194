7950a0b51afb1daa6b7b7cce3aaf00ad774b0390
Alvaro-SP <socop2412@gmail.com>
2022-10-14 23:18:21 -0600

    starting with the functions of the database

--- Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
+++ Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
@@ -1,12 +1,33 @@
 
 
 --! █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█   █▀▄▀█ █▄█   █▀▄ █▄▄
---! ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█   █░▀░█ ░█░   █▄▀ █▄█
+--! ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█   █░▀░█ ░█░   █▄▀ █▄█            ----------202000194-------
 
 
---* █▀▀ █░█ █▄░█ █▀▀ █ █▀█ █▄░█ █▀▀ █▀
---* █▀░ █▄█ █░▀█ █▄▄ █ █▄█ █░▀█ ██▄ ▄█
+--* █▀▀ █░█ █▄░█ █▀▀ ▀█▀ █ █▀█ █▄░█ █▀
+--* █▀░ █▄█ █░▀█ █▄▄ ░█░ █ █▄█ █░▀█ ▄█
 DELIMITER //
+DROP FUNCION IF EXISTS ValidarCorreo $$
+CREATE FUNCTION ValidarCorreo(direccion VARCHAR(45)) RETURNS BOOLEAN
+    DETERMINNISTIC
+    BEGIN
+    DECLARE valido BOOLEAN;
+    --* valido con el regex de CORREO
+    IF (SELECT REGEXP_INSTR(cad, '[^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})]')=0) THEN
+        SELECT TRUE INTO valido;
+    ELSE
+        SELECT FALSE INTO valido;
+    END IF;
+    RETURN (valido);
+    END $$
+BEGIN
+    DECLARE factorial INT;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+DROP FUNCION IF EXISTS RegistrarEstudiante $$
 CREATE FUNCTION RegistrarEstudiante
     (
     carnet BIGINT,
@@ -19,9 +40,12 @@ CREATE FUNCTION RegistrarEstudiante
     dpi BIGINT
     carrera INT 
     ) RETURNS VARCHAR(45)
+    DECLARE cdate DATETIME;
+    SET cdate = SSELECT now(); --* obtengo la fecha actual
+
 
     INSERT INTO usuarios (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos)
-                        VALUES (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos,);
+    VALUES (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,cdate,0);
 
     RETURN "ESTUDIANTE GUARDADO"
 
