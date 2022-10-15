dd9b4b586758d4706876912ec1c22dc8c7fa2397
Alvaro-SP <socop2412@gmail.com>
2022-10-15 09:40:11 -0600

    starting with the functions of the database

--- Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
+++ Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
@@ -13,7 +13,7 @@ CREATE FUNCTION ValidarCorreo(correo VARCHAR(45)) RETURNS BOOLEAN
     BEGIN
     DECLARE valido BOOLEAN;
     -- * valido con el regex de CORREO
-    IF (SELECT REGEXP_INSTR(correo, '(\w+@\w+(\.\w+)+)')) =1 THEN
+    IF (SELECT REGEXP_INSTR(correo, '^[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9_\-]@[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9]\.[a-zA-Z]{2,4}$')=1)  THEN
         SELECT TRUE INTO valido;
     ELSE
         SELECT FALSE INTO valido;
@@ -22,7 +22,7 @@ CREATE FUNCTION ValidarCorreo(correo VARCHAR(45)) RETURNS BOOLEAN
     END //
 DELIMITER ;
 
-
+-- ! █▄██▄██▄██▄██▄██▄██▄██▄██▄██▄█ REGISTRO DE ESTUDIANTES █▄██▄██▄██▄██▄██▄██▄██▄██▄██▄██▄█
 DELIMITER //
 DROP FUNCTION IF EXISTS RegistrarEstudiante //
 CREATE FUNCTION RegistrarEstudiante
@@ -53,4 +53,42 @@ CREATE FUNCTION RegistrarEstudiante
     RETURN "ESTUDIANTE GUARDADO";
     END//
 DELIMITER ;
-SELECT RegistrarEstudiante(2020001942,'Alvaro', 'Socop', '2001-12-24','gvg@gmail.com',55555555,'mlsw calle juan', 3034161730108,14 );
+
+
+SELECT RegistrarEstudiante(2020001942,'Alvaro', 'Socop', '2001-12-24','socop@gmail.com',55555555,'mlsw calle juan', 3034161730108,14 ) AS RESPUESTA_RegistrarEstudiante;
+
+
+-- ! █▄██▄██▄██▄██▄██▄██▄██▄██▄██▄█ REGISTRO DE DOCENTES █▄██▄██▄██▄██▄██▄██▄██▄██▄██▄██▄█
+DELIMITER //
+DROP FUNCTION IF EXISTS RegistrarEstudiante //
+CREATE FUNCTION RegistrarEstudiante
+    (  
+    carnet BIGINT,
+    nombres VARCHAR(45),
+    apellidos VARCHAR(45),
+    fecha_nacimiento DATETIME,
+    correo VARCHAR(45),
+    telefono INT,
+    direccion VARCHAR(45),
+    dpi BIGINT,
+    carrera INT 
+    ) RETURNS VARCHAR(65)
+    deterministic
+    BEGIN
+    DECLARE cdate DATETIME;
+    DECLARE temp BOOLEAN;
+    SET cdate = now(); -- * obtengo la fecha actual    
+    SET temp = ValidarCorreo(correo);
+    IF (temp = 0) THEN
+		RETURN 'ERROR DE CORREO VERIFICAR EL FORMATO DE CORREO';
+	END IF;
+
+    INSERT INTO ESTUDIANTE (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos)
+    VALUES (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,cdate,0);
+
+    RETURN "ESTUDIANTE GUARDADO";
+    END//
+DELIMITER ;
+
+
+SELECT RegistrarEstudiante(2020001942,'Alvaro', 'Socop', '2001-12-24','socop@gmail.com',55555555,'mlsw calle juan', 3034161730108,14 ) AS RESPUESTA_RegistrarEstudiante;
