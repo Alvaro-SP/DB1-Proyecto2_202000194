3ef452f71f7a2dcafb9ca8df2f10044c4d82cac7
Alvaro-SP <socop2412@gmail.com>
2022-10-14 23:41:04 -0600

    starting with the functions of the database

--- Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
+++ Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
@@ -7,19 +7,19 @@
 --* █▀▀ █░█ █▄░█ █▀▀ ▀█▀ █ █▀█ █▄░█ █▀
 --* █▀░ █▄█ █░▀█ █▄▄ ░█░ █ █▄█ █░▀█ ▄█
 DELIMITER //
-DROP FUNCION IF EXISTS ValidarCorreo $$
+DROP FUNCTION IF EXISTS ValidarCorreo //
 CREATE FUNCTION ValidarCorreo(direccion VARCHAR(45)) RETURNS BOOLEAN
-    DETERMINNISTIC
+    deterministic
     BEGIN
     DECLARE valido BOOLEAN;
-    --* valido con el regex de CORREO
+    -- * valido con el regex de CORREO
     IF (SELECT REGEXP_INSTR(cad, '[^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})]')=0) THEN
         SELECT TRUE INTO valido;
     ELSE
         SELECT FALSE INTO valido;
     END IF;
     RETURN (valido);
-    END $$
+    END //
 BEGIN
     DECLARE factorial INT;
 END//
@@ -27,7 +27,7 @@ DELIMITER ;
 
 
 DELIMITER //
-DROP FUNCION IF EXISTS RegistrarEstudiante $$
+DROP FUNCTION IF EXISTS RegistrarEstudiante //
 CREATE FUNCTION RegistrarEstudiante
     (
     carnet BIGINT,
@@ -35,24 +35,26 @@ CREATE FUNCTION RegistrarEstudiante
     apellidos VARCHAR(45),
     fecha_nacimiento DATETIME,
     correo VARCHAR(45),
-    telefono INT
+    telefono INT,
     direccion VARCHAR(45),
-    dpi BIGINT
+    dpi BIGINT,
     carrera INT 
     ) RETURNS VARCHAR(45)
+    deterministic
+    BEGIN
     DECLARE cdate DATETIME;
-    SET cdate = SSELECT now(); --* obtengo la fecha actual
-
+    DECLARE temp BOOLEAN;
+    SET cdate = now(); -- * obtengo la fecha actual
+    SET temp = ValidarCorreo(correo);
+    IF temp != 1 THEN
+        RETURN 'ERROR DE CORREO VERIFICAR EL FORMATO DE CORREO';
+    END IF;
 
     INSERT INTO usuarios (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos)
     VALUES (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,cdate,0);
 
-    RETURN "ESTUDIANTE GUARDADO"
+    RETURN "ESTUDIANTE GUARDADO";
 
-BEGIN
-    DECLARE factorial INT;
-
-
-END//
+    END//
 DELIMITER ;
 
