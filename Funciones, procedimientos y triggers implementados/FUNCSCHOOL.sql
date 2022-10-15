aad9d895b37739697c999af4fc6c622cffa3bb47
Alvaro-SP <socop2412@gmail.com>
2022-10-15 02:14:48 -0600

    starting with the functions of the database

--- Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
+++ Funciones, procedimientos y triggers implementados/FUNCSCHOOL.sql
@@ -8,28 +8,25 @@
 --* █▀░ █▄█ █░▀█ █▄▄ ░█░ █ █▄█ █░▀█ ▄█
 DELIMITER //
 DROP FUNCTION IF EXISTS ValidarCorreo //
-CREATE FUNCTION ValidarCorreo(direccion VARCHAR(45)) RETURNS BOOLEAN
+CREATE FUNCTION ValidarCorreo(correo VARCHAR(45)) RETURNS BOOLEAN
     deterministic
     BEGIN
     DECLARE valido BOOLEAN;
     -- * valido con el regex de CORREO
-    IF (SELECT REGEXP_INSTR(cad, '[^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})]')=0) THEN
+    IF (SELECT REGEXP_INSTR(correo, '(\w+@\w+(\.\w+)+)')) =1 THEN
         SELECT TRUE INTO valido;
     ELSE
         SELECT FALSE INTO valido;
     END IF;
     RETURN (valido);
     END //
-BEGIN
-    DECLARE factorial INT;
-END//
 DELIMITER ;
 
 
 DELIMITER //
 DROP FUNCTION IF EXISTS RegistrarEstudiante //
 CREATE FUNCTION RegistrarEstudiante
-    (
+    (  
     carnet BIGINT,
     nombres VARCHAR(45),
     apellidos VARCHAR(45),
@@ -39,22 +36,21 @@ CREATE FUNCTION RegistrarEstudiante
     direccion VARCHAR(45),
     dpi BIGINT,
     carrera INT 
-    ) RETURNS VARCHAR(45)
+    ) RETURNS VARCHAR(65)
     deterministic
     BEGIN
     DECLARE cdate DATETIME;
     DECLARE temp BOOLEAN;
-    SET cdate = now(); -- * obtengo la fecha actual
+    SET cdate = now(); -- * obtengo la fecha actual    
     SET temp = ValidarCorreo(correo);
-    IF temp != 1 THEN
-        RETURN 'ERROR DE CORREO VERIFICAR EL FORMATO DE CORREO';
-    END IF;
+    IF (temp = 0) THEN
+		RETURN 'ERROR DE CORREO VERIFICAR EL FORMATO DE CORREO';
+	END IF;
 
-    INSERT INTO usuarios (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos)
+    INSERT INTO ESTUDIANTE (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,fechacreacion,creditos)
     VALUES (carnet,nombres,apellidos,fecha_nacimiento,correo,telefono,direccion,dpi,carrera,cdate,0);
 
     RETURN "ESTUDIANTE GUARDADO";
-
     END//
 DELIMITER ;
-
+SELECT RegistrarEstudiante(2020001942,'Alvaro', 'Socop', '2001-12-24','gvg@gmail.com',55555555,'mlsw calle juan', 3034161730108,14 );
